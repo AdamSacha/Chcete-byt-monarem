97eb2a47914478ef582c1265c880bc7ee991cd0f
AdamSacha <81458669+AdamSacha@users.noreply.github.com>
2022-02-21 14:24:47 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,230 @@
+questions = [
+	{ question: "Vypočítejte 4³", answers: ["64", "16", "32", "88"], correct: "64" },
+{ question: "Co zničilo Pompeje?", answers: ["Sopka", "Požár", "Povodeň", "Tornádo"], correct: "Sopka" },
+{ question: "Kdo je první v abecedě ve třídě?", answers: ["Antonín Čemus", "Berenika Smíškova", "Max Zapp", "Linda Červinková"], correct: "Antonín Čemus" },
+{ question: "Kdo je poslední v abecedě ve třídě?", answers: ["Dorota Zemanová", "Lukáš Zákora", "Max Zapp", "Lukáš Fidrant"], correct: "Dorota Zemanová" },
+        { question: "Kdo je největší dealer?", answers: ["Michal K.", "Šimon", "Aneta", "Šimon"], correct: "Michal K." },
+        { question: "Jak se jmenuje věta o shodnosti trojúhelníku strana úhel strana?", answers: ["SUS", "ASS", "BAMK", "ABC"], correct: "SUS" },
+        { question: "Co je letní ovoce na VM?", answers: ["Vodový melun", "Velká malina", "Vaškova máma", "Velká muchomůrka"], correct: "Velká malina" },
+        { question: "Kdo pěstuje nivu?", answers: ["Michal, Adam, Jindra, Jenda", "Petra", "Vašek, Jindra", "Adam, No name"], correct: "Michal, Adam, Jindra, Jenda" },
+        { question: "Dokonči větu: Sto dvacet", answers: ["šest", "tři", "osm osm osm osm", "krát tvoje máma"], correct: "osm osm osm osm" },
+        { question: "Jaký je nejlepší typ sýra podle třídních gurmanů?", answers: ["Niva", "Karamel bér", "Eidam", "Ementál"], correct: "Niva" },
+        { question: "Kdo je a má přezdívku gentleman?", answers: ["Michal K.", "Adam Š.", "Honza M.", "Kuba K."], correct: "Michal K." },
+        { question: "Jak se jmenuje náš discord server?", answers: ["SEKUNDA SUPER MÍSTO™", "Prima discord", "Prima among us", "Among us <3"], correct: "SEKUNDA SUPER MÍSTO™" },
+        { question: "Jak se jmenuje naše třídní skupina?", answers: ["Psychiatrická léčebna", "Prima prima", "Prima sekunda", "Among us fanclub"], correct: "Psychiatrická léčebna" },
+        { question: "Kdy zemřel Svatý Václav?", answers: ["899", "935", "940", "956"], correct: "935" },
+        { question: "Kdo nechal založit Pražský hrad?", answers: ["Bořivoj I.", "Václav Synek", "Boleslav I.", "Rudolf II."], correct: "Bořivoj I." },
+        { question: "Kdo za všechno může?", answers: ["Michal", "Vašek", "Jenda", "Berenika"], correct: "Vašek" },
+        { question: "Kdo proti sobě bojoval v Peloponéských válkách?", answers: ["Athény a Sparta", "peloponéský spolek a athénský námořní spolek", "Řecko a Persie", "Athény a Řím"], correct: "peloponéský spolek a athénský námořní spolek" },
+        { question: "Který z těchto členů není rozvíjející?", answers: ["Přívlastek", "Předmět", "Doplněk", "Přísudek"], correct: "Přísudek" },
+        { question: "Kdo je v sekudně nejvíc sus?", answers: ["Šimon", "Filip", "Zbyněk", "Adam"], correct: "Adam" },
+        { question: "Kolik nohou má pavouk?", answers: ["8", "6", "2", "128"], correct: "8" },
+        { question: "Vypočítejte  8*8/4*7-39", answers: ["93", "71", "73", "79"], correct: "73" },
+        { question: "Kdo byl prvním prezidentem České Republiky?", answers: ["Tomáš Garrigue Masaryk", "Edvard Beneš", "Petra Nová", "Václav Havel"], correct: "Václav Havel" },
+        { question: "Kdy vznikla Česká Republika?", answers: ["1918", "1989", "1992", "1993"], correct: "1993" },
+        { question: "Kdy začala 2. světová válka?", answers: ["1940", "1939", "1945", "1914"], correct: "1939" },
+        { question: "Kdo jako poslední komentoval soutěž Chcete být monářem?", answers: ["Marek Vašut", "Lukáš Fidrant", "Zdeněk Izer", "Vladimír Čech"], correct: "Lukáš Fidrant" },
+        { question: "Kterou hlášku používá nejčastěji Tomáš Hradecký?", answers: ["chichichichá", "Tvoje máma", "Tvoje babička", "Adam je ipostor"], correct: "Tvoje máma" },
+        { question: "Kolik je 69% z 1000?", answers: ["420", "690", "69", "128"], correct: "690" },
+        { question: "Jak se jmenuje ikonická písnička od Ricka Aestlyho", answers: ["Rick roll", "Never gonna give you up", "Největší internetový trol", "menty"], correct: "Never gonna give you up" },
+        { question: "Jak se jmenuje ostrov jižně od austrálie", answers: ["Papua Nová Guinea", "Tasmánie", "Nový Zéland", "Madagaskar"], correct: "Tasmánie" },
+        { question: "Který pták si nestaví hnízdo?", answers: ["kukačka", "orel", "racek", "vlaštovka"], correct: "kukačka" },
+        { question: "Na kolika polokoulích se rozkládá afrika?", answers: ["na 5", "na 3", "na 4", "na 2"], correct: "na 4" },
+        { question: "Na kolik ostrovů se dělí Nový Zéland?", answers: ["na 3", "na 7", "na 2", "na 5"], correct: "na 2" },
+        { question: "Co je promile?", answers: ["desetina celku", "setina celku", "desetina procenta", "setina procenta"], correct: "desetina procenta" },
+        { question: "Který z těchto živočichů není Australský endemit?", answers: ["klokan", "ptakopysk", "aligátor", "koala"], correct: "aligátor" },
+        { question: "Co je to pixel?", answers: ["nejmenší obrazový bod na monitoru", "metoda na složení matematické rovnice", "vaškova babička", "Impostor z among us"], correct: "nejmenší obrazový bod na monitoru" },
+        { question: "Ve které fázi přestanou plazi růst?", answers: ["když zemřou", "rok a půl po vylíhnutí", "když vaškova babička udělá lívance", "po páření"], correct: "když zemřou" },
+        { question: " Rod slavníkovcu byl vyvražděn v roce:", answers: ["995", "895", "885", "896"], correct: "995" },
+        { question: "Z jakého zdroje pochází světlo měsíce?", answers: ["odráží se ze slunce", "V kamení na měsíci je obsažen fosfor", "někdo tam zapoměl zhasnout", "Je tam neon"], correct: "odráží se ze slunce" },
+        { question: "Zpěvák, legenda české counry muziky se jmenoval Michal:", answers: ["Tučný", "Holý", "David", "Zručný"], correct: "Tučný" },
+        { question: "Které přírodní katastrofy se měří richtrovou stupnicí?", answers: ["zemětřesení", "tornáda", "výbuchy sopek", "povodně"], correct: "zemětřesení" },
+        { question: "Jakou barvu mají květy lípy?", answers: ["žlutou", "bílou", "lehce namodralou", "lehce fialovou"], correct: "žlutou" },
+        { question: "Jak se jmenuje třídní učitel/ka sekundy?", answers: ["Jakub Skalák", "Olga Kloučková", "Jan Bašta", "Ivana Šimková"], correct: "Jakub Skalák" },
+        { question: "Která droga je obsažena v čaji?", answers: ["kofein", "thearubigin", " theaflavin", "polyfenol"], correct: "kofein" },
+        { question: "Co je nejtvrdším nerostem na světě ", answers: ["lonsdaleit", "diamant", "uhlí", "uhlík"], correct: "lonsdaleit" },
+        { question: "Co znamená že někdo vymřel po meči?", answers: ["V rodu není žádný mužský potomek", "V rodu není žádný ženský potomek", "vyvraždil je jiný rod", "Zabili se sami"], correct: "V rodu není žádný mužský potomek" },
+        { question: "Kdo byla práčata", answers: ["Dětští bojovníci v husitských bitvách", "děti které pomáhají s praním prádla", "děti které vyvolávaly pouliční bitky", "děti co stříleli prakem do sousedů"], correct: "Dětští bojovníci v husitských bitvách" },
+        { question: "Co je to píce", answers: ["krmivo pro dobytek", "upečené pečivo", "pec", "trouba "], correct: "krmivo pro dobytek" },
+        { question: "Co vznikne z cukru, když ho zahříváním rozpustíme?", answers: ["karamel", "sirup", "věc s příchutí skaženého mléka", "věc s příchutí normálního mléka"], correct: "karamel" },
+        { question: "Jak dlouho k nám kletí světlo ze slunce?", answers: ["8 minut", "30 minut", "12,5 minut", "3 minuty"], correct: "8 minut" },
+        { question: "Z rodu Slavníkovců pocházel náš známý světec. Jak se jmenoval", answers: ["sv. Vojtěch", "sv. Radovan", "sv. Filip", "sv. Vít"], correct: "sv. Vojtěch" }
+]
+    // Vzor : { question: "q15", answers: ["f15", "f15", "f15", "r15"], correct: "r15" },
+
+
+function showTitles() {
+    document.getElementById("titles").style.display = "flex"
+    document.getElementById("startScreen").style.display = "none";
+}
+
+function hideTitles() {
+    document.getElementById("titles").style.display = "none"
+    document.getElementById("startScreen").style.display = "block";
+}
+
+var answerAudio = ""
+score = 0;
+
+var health = 2
+    // pick 10 random questions from questions array
+    // randomize order of the anwsers array and order of the questions array
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+function pickQuestions() {
+    pickedQuestions = shuffle(questions).slice(0, 10);
+	questions.splice(0,10)
+
+    for (var i = 0; i < pickedQuestions.length; i++) {
+        pickedQuestions[i].answers = shuffle(pickedQuestions[i].answers);
+    }
+}
+
+
+function recolorQuestions() {
+    for (var i = 1; i < 5; i++) {
+        document.getElementById("answerBtn" + (i) + "Parent").classList.add("answers")
+        document.getElementById("answerBtn" + (i) + "Parent").classList.remove("disabled");
+        document.getElementById("answerBtn" + (i) + "Parent").classList.remove("answers")
+    }
+}
+
+function recolorTable() {
+    var table = document.getElementById("tableParent");
+    for (var i = 0; i < table.rows.length; i++) {
+        for (var j = 0; j < 2; j++) {
+            table.rows[i].children[j].style.color = "white";
+        }
+    }
+}
+
+function pickQuestion() {
+    if (pickedQuestions.length > 0) {
+        // pick random question from pickedQuestions array
+        var randomQuestion = Math.floor(Math.random() * pickedQuestions.length);
+        var chosenQuestion = pickedQuestions[randomQuestion];
+        // remove question from array
+        pickedQuestions.splice(randomQuestion, 1);
+        return (chosenQuestion);
+    } else { return ("no pickedQuestions left"); }
+}
+
+function endGame() {
+	  document.getElementById("tableParent").style.display = "none"
+		document.getElementById("healthDiv").style.display = "none"
+    document.getElementById("gameEnd").style.display = "block"
+    document.getElementById("gameSecondary").style.display = "none";
+    document.getElementById("gameoverText").style.display = "block";
+		if(score == 0) {document.getElementById("gameoverText").innerHTML = "Konec, prohráli jste, " + "výhra: 0 Kč"
+
+		}
+    else if (score == 10) {
+        document.getElementById("gameoverText").innerHTML = "Konec, vyhráli jste, " + "výhra: " + document.getElementById("scoreNr" + (score - 1)).children[1].innerHTML;
+    } else {
+        document.getElementById("gameoverText").innerHTML = "Konec, prohráli jste, " + "výhra: " + document.getElementById("scoreNr" + (score - 1)).children[1].innerHTML;
+    }
+}
+
+function nextQuestion() {
+    chosenQuestion = pickQuestion();
+    if (chosenQuestion == "no pickedQuestions left") {
+        endGame();
+
+    } else {
+        recolorQuestions();
+        document.getElementById("question").innerHTML = chosenQuestion[Object.keys(chosenQuestion)[0]]
+
+        for (var i = 0; i < 4; i++) {
+            //add an event listener to each answer button to run checkAnswer function
+            document.getElementById("answerBtn" + (i + 1)).innerHTML = chosenQuestion[Object.keys(chosenQuestion)[1]][i];
+
+        }
+    }
+}
+
+// 
+
+function checkAnswer(answer) {
+    var audioVariation = Math.floor(Math.random() * 3) + 1
+    if (chosenQuestion[Object.keys(chosenQuestion)[2]] == chosenQuestion[Object.keys(chosenQuestion)[1]][answer]) {
+        score++;
+        answerAudio = new Audio("sounds/correct-" + audioVariation + ".ogg")
+        answerAudio.play()
+        console.log("correct");
+        for (var i = 0; i < 4; i++) {
+            document.getElementById("answerBtn" + String(i + 1)).style.display = "block";
+        }
+        recolorTable();
+        for (var i = 0; i < 2; i++) {
+            document.getElementById("scoreNr" + (score - 1)).children[i].style.color = "#ff4500";
+        }
+        nextQuestion();
+    } else {
+        answerAudio = new Audio("sounds/wrong-" + audioVariation + ".ogg")
+        answerAudio.play()
+        health--
+        if (health == 0) {
+            document.getElementById("health").innerHTML = health;
+            endGame();
+        } else {
+            document.getElementById("health").innerHTML = health;
+        }
+    }
+}
+
+function fiftyfifty() {
+    // pick 2 random buttons and if they are not the correct answer, remove them, if not keep trying until 2 buttons are left
+    var randomButton1 = Math.floor(Math.random() * 4);
+    var randomButton2 = Math.floor(Math.random() * 4);
+    while (randomButton1 == randomButton2) {
+        randomButton2 = Math.floor(Math.random() * 4);
+    }
+    if (chosenQuestion[Object.keys(chosenQuestion)[2]] != chosenQuestion[Object.keys(chosenQuestion)[1]][randomButton1] && chosenQuestion[Object.keys(chosenQuestion)[2]] != chosenQuestion[Object.keys(chosenQuestion)[1]][randomButton2]) {
+        document.getElementById("answerBtn" + (randomButton1 + 1)).style.display = "none";
+        document.getElementById("answerBtn" + (randomButton2 + 1)).style.display = "none";
+
+        document.getElementById("answerBtn" + (randomButton1 + 1) + "Parent").classList.add("disabled") && classList.remove("answers");
+        document.getElementById("answerBtn" + (randomButton2 + 1) + "Parent").classList.add("disabled") && classList.remove("answers");
+        document.getElementById("fiftyfiftyBtn" && "fiftyfiftyBtnParent").classList.add("disabled") && classList.remove("answers");
+    } else {
+        fiftyfifty();
+    }
+}
+
+function start() {
+    score = 0
+    health = 2
+    for (var i = 1; i <= 4; i++) {
+        document.getElementById("answerBtn" + (i)).style.display = "flex";
+        document.getElementById("answerBtn" + (i)).style.justifyContent = "center";
+    }
+
+    document.getElementById("fiftyfiftyBtnParent").classList.remove("disabled");
+		document.getElementById("tableParent").style.display = "block"
+    document.getElementById("gameEnd").style.display = "none";
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("gameMain").style.display = "block";
+    document.getElementById("gameSecondary").style.display = "block";
+		document.getElementById("healthDiv").style.display = "block"
+    document.getElementById("health").innerHTML = health;
+    pickQuestions();
+    recolorTable();
+    nextQuestion();
+}
